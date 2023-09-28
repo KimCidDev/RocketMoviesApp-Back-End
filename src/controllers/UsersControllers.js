faaeaf85f7f991fd326b6ebe16ca6687d02e0485
Kim <cid.kim@gmail.com>
2023-09-27 21:08:00 -0600

    user check on userControllers.create. if not throw AppError

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,11 +1,18 @@
 const AppError = require('../utils/AppError');
+const sqliteConnection = require('../database/sqlite');
 
 class UsersControllers {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    if (!name) {
-      throw new AppError('Você esqueceu o nome', 401);
+    const database = await sqliteConnection();
+    const checkIfUserExists = await database.get(
+      'SELECT FROM users WHERE email = (?)',
+      email
+    );
+
+    if (!checkIfUserExists) {
+      throw new AppError('O usuário não existe');
     }
 
     response.status(201).json({ name, email, password });
