cf493f67e6cb7a0ec514d595813176706386428c
Kim <cid.kim@gmail.com>
2023-09-29 07:16:13 -0600

    password compare created

--- src/controllers/UsersControllers.js
+++ src/controllers/UsersControllers.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 const AppError = require('../utils/AppError');
 const sqliteConnection = require('../database/sqlite');
 
@@ -27,11 +27,15 @@ class UsersControllers {
   }
 
   async update(request, response) {
-    const { name, email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
     const user = await database.get('SELECT * FROM USERS WHERE id = (?)', [id]);
+
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
+    }
 
     const userWithUpdatedEmail = await database.get(
       'SELECT * FROM USERS WHERE email = (?)',
@@ -45,16 +49,32 @@ class UsersControllers {
     user.name = name;
     user.email = email;
 
+    if (password && !old_password) {
+      throw new AppError('Você precisa informar a senha antiga');
+    }
+
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if (!checkOldPassword) {
+        throw new AppError('A senha antiga não confere');
+      }
+
+      user.password = hash(password, 8);
+    }
+
     await database.run(
       `
     UPDATE users SET
     name = ?,
     email = ?,
-    updated_at = ?,
-    WHERE id = ?`[(user.name, user.email, new Date(), id)]
+    password = ?,
+    updated_at = ?
+    WHERE id = ?`,
+      [user.name, user.email, user.password, new Date(), id]
     );
 
-    return response.json({});
+    return response.json();
   }
 }
 
